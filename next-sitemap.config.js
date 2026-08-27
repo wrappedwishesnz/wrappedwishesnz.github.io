9050717f6f2d265b047b7721a725a038427fc187
Robin Arulanantham <robin.arulanantham@infometrics.co.nz>
2026-08-28 00:32:32 +1200

    Add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteUrl: "https://www.wrappedwishes.nz",
+  generateRobotsTxt: true,
+  changefreq: "monthly",
+  priority: 1,
+};
--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-  basePath: process.env.NODE_ENV === "production" ? "/wrappedwishes" : "",
 };
 
 export default nextConfig;
